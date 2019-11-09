5535add3106e9084c077cbc35ab8dd2983195487
Albrecht Eckardt <albrecht.eckardt73@gmail.com>
2019-11-10 00:03:17 +0100

    Simplify Windows screenshot function. It was done like this because of the combined capture + compress function, which is now deleted.

--- image/screenshot_win.cpp
+++ image/screenshot_win.cpp
@@ -8,11 +8,21 @@
 
 #include "screenshot.h"
 
-bool _captureRect(const QRect &rect, QImage::Format format, char *&data, HBITMAP &hbmp, int &image_size)
+QImage captureDesktop(QImage::Format format)
+{
+    HDC hScreenDC = GetDC(nullptr);
+    int width = GetDeviceCaps(hScreenDC, HORZRES);
+    int height = GetDeviceCaps(hScreenDC, VERTRES);
+    ReleaseDC(nullptr, hScreenDC);
+
+    return captureRect({0, 0, width, height}, format);
+}
+
+QImage captureRect(const QRect &rect, QImage::Format format)
 {
     if (rect.width() < 1 || rect.height() < 1 ||
         (format != QImage::Format_RGB888 && format != QImage::Format_RGB32)) {
-        return false;
+        return QImage();
     }
 
     HDC hScreenDC = GetDC(nullptr);
@@ -30,19 +40,18 @@ bool _captureRect(const QRect &rect, QImage::Format format, char *&data, HBITMAP
     if (format == QImage::Format_RGB888) {
         bytes_per_line = ((rect.width() * 3) + 3) & 0xfffffc;
         bih.biBitCount = 24;
-    }
-    else if (format == QImage::Format_RGB32) {
+    } else if (format == QImage::Format_RGB32) {
         bytes_per_line = rect.width() * 4;
         bih.biBitCount = 32;
-    }
-    else {
-        return false;
+    } else {
+        return QImage();
     }
 
     bih.biSizeImage = static_cast<DWORD>(rect.height() * bytes_per_line);
-    image_size = bih.biSizeImage;
 
-    hbmp = CreateDIBSection(nullptr, reinterpret_cast<LPBITMAPINFO>(&bih), DIB_RGB_COLORS, reinterpret_cast<void **>(&data), nullptr, 0);
+    char* data;
+
+    HBITMAP hbmp = CreateDIBSection(nullptr, reinterpret_cast<LPBITMAPINFO>(&bih), DIB_RGB_COLORS, reinterpret_cast<void **>(&data), nullptr, 0);
 
     HGDIOBJ hOldObj = SelectObject(hMemoryDC, static_cast<HGDIOBJ>(hbmp));
     BitBlt(hMemoryDC, 0, 0, rect.width(), rect.height(), hScreenDC, rect.x(), rect.y(), SRCCOPY | CAPTUREBLT);
@@ -50,29 +59,6 @@ bool _captureRect(const QRect &rect, QImage::Format format, char *&data, HBITMAP
 
     DeleteDC(hMemoryDC);
     ReleaseDC(nullptr, hScreenDC);
-
-    return true;
-}
-
-QImage captureDesktop(QImage::Format format)
-{
-    HDC hScreenDC = GetDC(nullptr);
-    int width = GetDeviceCaps(hScreenDC, HORZRES);
-    int height = GetDeviceCaps(hScreenDC, VERTRES);
-    ReleaseDC(nullptr, hScreenDC);
-
-    return captureRect({0, 0, width, height}, format);
-}
-
-QImage captureRect(const QRect &rect, QImage::Format format)
-{
-    char *data;
-    HBITMAP hbmp;
-    int image_size;
-
-    if (!_captureRect(rect, format, data, hbmp, image_size)) {
-        return QImage();
-    }
 
     return QImage(reinterpret_cast<uchar *>(data), rect.width(), rect.height(), format, [](void *hbmp) { DeleteObject(hbmp); }, hbmp);
 }
