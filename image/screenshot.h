583843a212734bed4552e2e1f39a4e09b4b32dc2
Albrecht Eckardt <gladamyran@gmail.com>
2020-04-08 11:45:40 +0200

    Minor change

--- image/screenshot.h
+++ image/screenshot.h
@@ -8,10 +8,10 @@
 void captureDesktop(QImage &dest, int linesize_align = 0, QImage::Format format = QImage::Format_RGB32);
 void captureRect(const QRect &, QImage &dest, int linesize_align = 0, QImage::Format format = QImage::Format_RGB32);
 
-inline QImage captureDesktop(int linesize_align = 0, QImage::Format format = QImage::Format_RGB32)
-{ QImage dest; captureDesktop(dest, linesize_align, format); return dest; }
+inline QImage captureDesktop(QImage::Format format = QImage::Format_RGB32)
+{ QImage dest; captureDesktop(dest, 0, format); return dest; }
 
-inline QImage captureRect(const QRect &rect, int linesize_align = 0, QImage::Format format = QImage::Format_RGB32)
-{ QImage dest; captureRect(rect, dest, linesize_align, format); return dest; }
+inline QImage captureRect(const QRect &rect, QImage::Format format = QImage::Format_RGB32)
+{ QImage dest; captureRect(rect, dest, 0, format); return dest; }
 
 #endif // SCREENSHOT_H
