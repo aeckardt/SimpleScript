1ee1e53d150880da46d8c82c261f4bebe62da913
Albrecht Eckardt <gladamyran@gmail.com>
2020-04-10 16:51:16 +0200

    Remove error in .travis.yml

--- tools/download_package.sh
+++ tools/download_package.sh
@@ -4,7 +4,6 @@ repo_path=$2
 release=$3
 target_dir=$4
 download_url="https://codeload.github.com/$repo_path/tar.gz/$release"
-echo "download_url: $download_url"
 target_file="$release.tar.gz"
 curl -o $target_file $download_url
 tar -xvf $target_file
