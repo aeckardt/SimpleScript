c5bd887ba3e21892353c39b6f3d65d70da749faa
Albrecht Eckardt <gladamyran@gmail.com>
2020-04-10 17:22:15 +0200

    Remove build error

--- tools/download_package.sh
+++ tools/download_package.sh
@@ -7,6 +7,10 @@ download_url="https://codeload.github.com/$repo_path/tar.gz/$release"
 target_file="$release.tar.gz"
 curl -o $target_file $download_url
 tar -xvf $target_file
+if [[ ! -d "external" ]]
+then
+  mkdir external
+fi
 mkdir external/$repo_name
 mv $target_dir/$5/* external/$repo_name
 rm -rf $target_dir
