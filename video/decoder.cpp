aae5fb1e6753ea79851518aa8aba744412fd7129
aeckardt <44362175+aeckardt@users.noreply.github.com>
2020-04-15 10:45:18 +0200

    Minor change

--- video/decoder.cpp
+++ video/decoder.cpp
@@ -1,10 +1,10 @@
 #include "decoder.h"
 
 extern "C" {
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <libavutil/imgutils.h>
-#include <libswscale/swscale.h>
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libavutil/imgutils.h"
+#include "libswscale/swscale.h"
 }
 
 VideoDecoder::VideoDecoder() :
