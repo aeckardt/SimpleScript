80ea70b4f042fbdea7f5a229ffcf4945e68a6bc4
Albrecht Eckardt <gladamyran@gmail.com>
2020-04-18 13:53:15 +0200

    Update build_project.sh

--- tools/build_project.sh
+++ tools/build_project.sh
@@ -4,14 +4,8 @@ mkdir build-Release
 qmake SimpleScript.pro -o build-Release
 cd build-Release
 echo "config is: $config"
-if [[ $config -eq "MacOS" ]]
-then
-  make
-fi
-if [[ $config -eq "Windows" ]]
-then
-  mingw32-make
-fi
+if [[ $config -eq "MacOS" ]] then make
+elif [[ $config -eq "Windows" ]] then mingw32-make fi
 result=$?
 cd ..
 exit $result
