483136aa68f8cfac797163be498a189902d5b945
Albrecht Eckardt <gladamyran@gmail.com>
2020-04-18 14:04:24 +0200

    Update build_project.sh

--- tools/build_project.sh
+++ tools/build_project.sh
@@ -2,8 +2,9 @@
 mkdir build-Release
 qmake SimpleScript.pro -o build-Release
 cd build-Release
-if [[ $CONFIG -eq "MacOS" ]]; then make; fi;
-if [[ $CONFIG -eq "Windows" ]]; then mingw32-make; fi;
+if [[ $CONFIG -eq "MacOS" ]]; then make_cmd=(make); fi;
+if [[ $CONFIG -eq "Windows" ]]; then make_cmd=(mingw32-make); fi;
+"${make_cmd[@]}"
 result=$?
 cd ..
 exit $result
