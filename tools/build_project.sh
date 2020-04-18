a64b5a99083a9fe8fd450495f9f29c691617b400
Albrecht Eckardt <gladamyran@gmail.com>
2020-04-18 14:20:42 +0200

    Adjust CI scripts

--- tools/build_project.sh
+++ tools/build_project.sh
@@ -2,8 +2,6 @@
 mkdir build-Release
 qmake SimpleScript.pro -o build-Release
 cd build-Release
-if [[ $CONFIG -eq "MacOS" ]]; then make_cmd=(make); fi;
-if [[ $CONFIG -eq "Windows" ]]; then make_cmd=(mingw32-make); fi;
 "${make_cmd[@]}"
 result=$?
 cd ..
--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 cd tests
-if [[ ! -d "build-Release" ]]
-then
-  mkdir build-Release
-fi
+if [[ ! -d "build-Release" ]]; then mkdir build-Release; fi;
 qmake tests.pro -o build-Release
 cd build-Release
-if [[ $config -eq "MacOS" ]]
-then
-  make
-elif [[ $config -eq "Windows" ]]
-then
-  mingw32-make
-fi
+"$make_cmd"
 result=$?
-if [[ $result -eq 0 ]]
-then
-  ./tests
-  result=$?
-fi
+if [[ $result -eq 0 ]]; then
+  ./tests;
+  result=$?;
+fi;
 cd ../..
 exit $result
