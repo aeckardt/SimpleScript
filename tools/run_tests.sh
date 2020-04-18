cefba2354524684b45b6b7a8c6191a22a1d7eada
Albrecht Eckardt <gladamyran@gmail.com>
2020-04-18 14:25:46 +0200

    Minor change Update run_tests.sh

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -3,7 +3,7 @@ cd tests
 if [[ ! -d "build-Release" ]]; then mkdir build-Release; fi;
 qmake tests.pro -o build-Release
 cd build-Release
-"$make_cmd"
+"${make_cmd[@]}"
 result=$?
 if [[ $result -eq 0 ]]; then
   ./tests;
