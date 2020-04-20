2f06a27d673e8131eebcb7831464301753263270
aeckardt <44362175+aeckardt@users.noreply.github.com>
2020-04-20 15:07:11 +0200

    Refactor VideoFile, see #11

--- video/videofile.h
+++ video/videofile.h
@@ -8,7 +8,6 @@ class VideoFile
 {
 public:
     VideoFile() {}
-    VideoFile(const VideoFile& src) { *this = src; }
     VideoFile(const QString &file_name) { file_path = file_name; }
 
     void createTemporary() { temp_file.open(); file_path = temp_file.fileName(); }
@@ -16,21 +15,10 @@ public:
     const QString &fileName() const { return file_path; }
     void save(const QString &file_name) const { QFile::copy(file_path, file_name); }
 
-    VideoFile &operator=(const VideoFile &src) { file_path = src.file_path; return *this; }
-
 private:
     QTemporaryFile temp_file;
     QString file_path;
-
-    friend VideoFile temporaryVideo();
 };
-
-inline VideoFile temporaryVideo()
-{
-    VideoFile video_file;
-    video_file.createTemporary();
-    return video_file;
-}
 
 #endif // VIDEOFILE_H
 
