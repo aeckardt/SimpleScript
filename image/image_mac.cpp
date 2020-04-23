60f3c8dd2a0df291dd04a830752fd80986d06f40
aeckardt <44362175+aeckardt@users.noreply.github.com>
2020-04-23 13:11:10 +0200

    Fix build error on MacOS

--- image/image_mac.cpp
+++ image/image_mac.cpp
@@ -4,7 +4,7 @@
 
 #include <ApplicationServices/ApplicationServices.h>
 
-inline void captureFromRef(Image &dest, CGImageRef &image_ref, bool can_reallocate)
+inline void captureFromRef(Image &dest, CGImageRef &image_ref)
 {
     int width = static_cast<int>(CGImageGetWidth(image_ref));
     int height = static_cast<int>(CGImageGetHeight(image_ref));
@@ -37,7 +37,7 @@ void Image::captureDesktop()
     CGDirectDisplayID main_display = CGMainDisplayID();
     CGImageRef image_ref = CGDisplayCreateImage(main_display);
 
-    captureFromRef(*this, image_ref, can_reallocate);
+    captureFromRef(*this, image_ref);
 }
 
 void Image::captureRect(const QRect &rect)
@@ -45,7 +45,7 @@ void Image::captureRect(const QRect &rect)
     CGDirectDisplayID main_display = CGMainDisplayID();
     CGImageRef image_ref = CGDisplayCreateImageForRect(main_display, CGRectMake(rect.x(), rect.y(), rect.width(), rect.height()));
 
-    captureFromRef(*this, image_ref, can_reallocate);
+    captureFromRef(*this, image_ref);
 }
 
 #endif
