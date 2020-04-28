edf7cebb310ec5eed1a472cf166c5578e3a0057b
aeckardt <gladamyran@gmail.com>
2020-04-28 11:50:07 +0200

    Fix script To make unit test run on MacOS

--- ci-tools/run_tests.sh
+++ ci-tools/run_tests.sh
@@ -3,10 +3,14 @@ cd tests
 if [[ ! -d "build-Release" ]]; then mkdir build-Release; fi;
 qmake tests.pro -o build-Release
 cd build-Release
-"${make_cmd[@]}" -f Makefile.Release
+"${make_cmd[@]}"
 result=$?
 if [[ $result -eq 0 ]]; then
-  ./release/tests;
+  if [[ -f ./tests ]]; then
+    ./tests;
+  elif [[ -f ./release/tests ]]; then
+    ./release/tests;
+  fi;
   result=$?;
 fi;
 cd ../..
