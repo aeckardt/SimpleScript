f33a974838fe9d51f4e668474e689781f8f1fb90
aeckardt <gladamyran@gmail.com>
2020-04-28 11:17:04 +0200

    Update run_tests.sh The new qmake seems to put the executables in a subdirectory

--- ci-tools/run_tests.sh
+++ ci-tools/run_tests.sh
@@ -3,11 +3,10 @@ cd tests
 if [[ ! -d "build-Release" ]]; then mkdir build-Release; fi;
 qmake tests.pro -o build-Release
 cd build-Release
-"${make_cmd[@]}"
+"${make_cmd[@]}" -f Makefile.Release
 result=$?
 if [[ $result -eq 0 ]]; then
-  ls -all
-  ./tests;
+  ./release/tests;
   result=$?;
 fi;
 cd ../..
